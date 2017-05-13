223cdc6a1798c10dda00bc18214ac2849d518c50
KingsleyChung <kingsleychung@outlook.com>
2017-05-13 21:39:41 +0800

    add go back button and modify titlebar appearance

--- front-end/TennisCourt/TennisCourt/App.xaml.cs
+++ front-end/TennisCourt/TennisCourt/App.xaml.cs
@@ -8,6 +8,8 @@ using Windows.ApplicationModel.Activation;
 using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -48,6 +50,9 @@ namespace TennisCourt
 #endif
             Frame rootFrame = Window.Current.Content as Frame;
 
+            //返回键
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested += BackRequested;
+
             // 不要在窗口已包含内容时重复应用程序初始化，
             // 只需确保窗口处于活动状态
             if (rootFrame == null)
@@ -64,7 +69,13 @@ namespace TennisCourt
 
                 // 将框架放在当前窗口中
                 Window.Current.Content = rootFrame;
-                CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+                var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+                var appTitleBar = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().TitleBar;
+
+                //界面延伸至标题栏
+                coreTitleBar.ExtendViewIntoTitleBar = true;
+                //标题栏按钮透明
+                appTitleBar.ButtonBackgroundColor = Colors.Transparent;
             }
 
             if (e.PrelaunchActivated == false)
@@ -80,6 +91,28 @@ namespace TennisCourt
                 }
                 // 确保当前窗口处于活动状态
                 Window.Current.Activate();
+
+                // 判断是否需要返回键
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = rootFrame.CanGoBack ? AppViewBackButtonVisibility.Visible : Windows.UI.Core.AppViewBackButtonVisibility.Collapsed;
+                rootFrame.Navigated += OnNavigated;
+            }
+        }
+
+        private void OnNavigated(object sender, NavigationEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = ((Frame)sender).CanGoBack ?
+                AppViewBackButtonVisibility.Visible : AppViewBackButtonVisibility.Collapsed;
+        }
+
+        private void BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null)
+                return;
+            if (rootFrame.CanGoBack && e.Handled == false)
+            {
+                e.Handled = true;
+                rootFrame.GoBack();
             }
         }
 
