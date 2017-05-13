47f65aa17b64556d8ce799a2efa93a23733366eb
钟梓灏 <1017642638@qq.com>
2017-05-13 17:30:59 +0800

    detail

--- front-end/TennisCourt/TennisCourt/DetailPage.xaml.cs
+++ front-end/TennisCourt/TennisCourt/DetailPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -52,6 +54,7 @@ namespace TennisCourt
             var serverScore = "0";
             var receiverScore = "0";
             ViewModel.AddGameScore(gameID, totalGames, serverName, receiverName, ballFlag, serverSet, receiverSet, serverScore, receiverScore);
+            //ViewModel.SelectGameScore = ViewModel.AllGameScore.ElementAt(ViewModel.AllGameScore.Count - 1);
         }
 
         private void ScoreDisplay_ItemClick(object sender, ItemClickEventArgs e)
@@ -59,12 +62,66 @@ namespace TennisCourt
 
         }
 
-        private void Server_Add_Click(object sender, RoutedEventArgs e)
+        private async void Server_Add_Click(object sender, RoutedEventArgs e)
         {
+            var select = ((Button)e.OriginalSource).DataContext as Models.Score;
+            var selectgame = select.TotalGames;
+            var selectserverset = select.ServerSet;
+            var selectreceiverset = select.ReceiverSet;
+            var selectserverscore = select.ServerScore;
+            var selectreceiverscore = select.ReceiverScore;
 
+            if (selectserverscore == "0") selectserverscore = "15";
+            else if (selectserverscore == "15") selectserverscore = "30";
+            else if (selectserverscore == "30") selectserverscore = "40";
+            else if (selectserverscore == "40" && selectreceiverscore == "40") selectserverscore = "Ad";
+            else if (selectserverscore == "40" && selectreceiverscore == "Ad")
+            {
+                selectserverscore = "40";
+                selectreceiverscore = "40";
+            }
+            else if (selectserverscore == "Ad" || (selectserverscore == "40" && selectreceiverscore != "40"))
+            {
+                selectserverscore = "0";
+                int set = int.Parse(selectserverset);
+                set++;
+                selectserverset = set.ToString();
+            }
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    //var result = ViewModel.SelectGameScore.ServerSet + "-" + ViewModel.SelectGameScore.ReceiverSet;
+                    
+                    var kvp = new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string,string>("matchId", ViewModel.SelectSpecialSet.SetID),
+                        new KeyValuePair<string,string>("game", selectgame.ToString()),
+                        new KeyValuePair<string,string>("result", selectserverset + "-" + selectreceiverset),
+                        new KeyValuePair<string,string>("score", selectserverscore + "-" + selectreceiverscore)
+                    };
+                    HttpResponseMessage response = await client.PostAsync("http://localhost:3000/changescore", new FormUrlEncodedContent(kvp));
+                    if (response.EnsureSuccessStatusCode().StatusCode.ToString().ToLower() == "ok")
+                    {
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        var gameinfo = JObject.Parse(responseBody);
+                        if ((string)gameinfo["ok"] != "0")
+                        {
+                            
+                        }
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    await new MessageDialog(ex.Message).ShowAsync();
+                }
+            }
+
+            
         }
 
-        private void Receiver_Add_Click(object sender, RoutedEventArgs e)
+        private async void Receiver_Add_Click(object sender, RoutedEventArgs e)
         {
 
         }
